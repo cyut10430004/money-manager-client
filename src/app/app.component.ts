68b2baff85d38b6a32e262f04690094bdb98d29c
peng <cxz0917001997@gmail.com>
2020-05-13 17:20:00 +0800

    test api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'money-manager-client77';
+  constructor(
+    private http: HttpClient
+  ) {
+    this.http.get('https://peng-money-manager-server.herokuapp.com/api/Test/hello').subscribe(res => console.log(res));
+  }
 }
